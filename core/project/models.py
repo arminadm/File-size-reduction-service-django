e8fd09dbebfebeda40960623402ed18ea031fc37
arminadm <armindarabimahboub@gmail.com>
2022-11-27 22:37:31 +0330

    abort 1: ffmpeg no such file or directory

--- core/project/models.py
+++ core/project/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .task import convert_video
 
 # Create your models here.
 class UploadedFiles(models.Model):
@@ -12,3 +15,9 @@ class UploadedFiles(models.Model):
 
     def __str__(self):
         return f"uploaded file: id={self.id}"
+    
+
+@receiver(post_save, sender=UploadedFiles)
+def start_convert(sender, instance, created, **kwargs):
+    if created:
+        convert_video.delay(instance.video_org.name)
--- core/project/task.py
+++ core/project/task.py
@@ -1,7 +1,26 @@
 from celery import shared_task
 from time import sleep
+import ffmpeg
 
 @shared_task
-def send_email():
-    sleep(3)
-    print('EMAIL SEND SUCCESSFULLY')
+def convert_video(video_link):
+    video_name = video_link.split('videos/org/')[1]
+    # print("##############################################1")
+    # stream = ffmpeg.input(video_link)
+    # print("##############################################2")
+    # stream = ffmpeg.hflip(stream)
+    # print("##############################################3")
+    # stream = ffmpeg.output(stream, f"videos240{video_name}")
+    # print("##############################################4")
+    # ffmpeg.run(stream)
+    # print("##############################################5")
+    # print('#######################################')
+    (
+        ffmpeg
+        .input('input.mp4')
+        .hflip()
+        .output('output.mp4')
+        .run()
+    )   
+    print(video_name)
+    print(video_link)
