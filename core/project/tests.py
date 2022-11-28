169fae680f5fa6c29762d078b9282b66da930799
arminadm <armindarabimahboub@gmail.com>
2022-11-28 07:31:38 +0330

    ffmpeg integration test finished

--- core/project/tests.py
+++ core/project/tests.py
@@ -1,3 +1,16 @@
+import os    
 from django.test import TestCase
+from datetime import datetime
+from .models import UploadedFiles
 
-# Create your tests here.
+class TestFFMPEG(TestCase):
+    def setUp(self):
+        self.test_video = "./videos/test/input.mp4"
+
+    def test_ffmpeg_integration(self, res=240):
+        video_name = "input.mp4"
+        out_path = f"./videos/{res}/{video_name}"
+        command_str = f"ffmpeg -i {self.test_video} -vf scale={res}:{res} {out_path}"
+        result = os.system(command_str)
+        self.assertTrue((result == 0) and (out_path == "./videos/240/input.mp4"))
+    
