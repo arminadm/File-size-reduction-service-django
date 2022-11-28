820abe7d343ab337a6096c4ddab05640fd96ba70
arminadm <armindarabimahboub@gmail.com>
2022-11-28 07:53:44 +0330

    redis test added

--- core/project/tests.py
+++ core/project/tests.py
@@ -1,4 +1,5 @@
 import os    
+from redis import Redis, exceptions
 from django.test import TestCase
 from datetime import datetime
 from .models import UploadedFiles
@@ -13,4 +14,26 @@ class TestFFMPEG(TestCase):
         command_str = f"ffmpeg -i {self.test_video} -vf scale={res}:{res} {out_path}"
         result = os.system(command_str)
         self.assertTrue((result == 0) and (out_path == "./videos/240/input.mp4"))
+    
+    def test_ffmpeg_integration(self, res=240):
+        video_name = "input.mp4"
+        out_path = f"./videos/{res}/{video_name}"
+        command_str = f"ffmpeg -i {self.test_video} -vf scale={res}:{res} {out_path}"
+        result = os.system(command_str)
+        self.assertTrue((result == 0) and (out_path == "./videos/240/input.mp4"))
+
+        
+class Test_Redis(TestCase):
+    def is_redis_available(self, r):
+        try:
+            r.ping()
+            print("Successfully connected to redis")
+        except (exceptions.ConnectionError, ConnectionRefusedError):
+            print("Redis connection error!")
+            return False
+        return True
+    
+    def test_is_redis_available(self):
+        r = Redis(host='localhost',port=6379,db=1)
+        self.assertTrue(self.is_redis_available(r))
     
